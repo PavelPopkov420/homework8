c6bcc982deff8c12f70cdb8e2469ac7c51ea6c74
Pavel Popkov <goil2011@yandex.ru>
2022-08-29 22:29:03 +0300

    task58

---
+++ task58/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrixA = new int[n, m];
+int[,] matrixB = new int[n, m];
+
+int[,] matrixC = new int[n, m];
+
+for (int i = 0; i< n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+    matrixA[i, j] = new Random().Next(-10, 10);
+    Console.Write(matrixA[i,j] + " \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i< n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+    matrixB[i, j] = new Random().Next(-10, 10);
+    Console.Write(matrixB[i,j] + " \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {        
+         matrixC[i, j] = matrixA[i, j] * matrixB[i, j];
+        Console.Write(matrixC[i,j] + " \t");
+    }
+     Console.WriteLine();
+}
