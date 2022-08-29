bfa3b66ca6eabb4c4176f231b3dc3fa746c705ad
Pavel Popkov <goil2011@yandex.ru>
2022-08-29 22:25:30 +0300

    task54

---
+++ task54/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+for (int i = 0; i< n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+    matrix[i, j] = new Random().Next(-10, 100);
+    Console.Write(matrix[i,j] + " \t");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m - 1; j++)
+    {
+        for (int z = 0; z < m - 1; z++)
+        {
+            if (matrix[i, z] > matrix[i, z + 1])
+            {
+                int temp = 0;
+                temp = matrix[i, z];
+                matrix[i, z] = matrix[i, z + 1];
+                matrix[i, z + 1] = temp;
+            }
+           
+        } 
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i< n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+    Console.Write(matrix[i,j] + " \t");
+    }
+    Console.WriteLine();
+}
