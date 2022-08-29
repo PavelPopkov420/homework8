4285f72ad7f73b0e37899578d6ea301299269e8f
Pavel Popkov <goil2011@yandex.ru>
2022-08-29 22:30:50 +0300

    task62

---
+++ task62/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+
+int num = 1;
+int a = 0;
+int b = 0;
+
+while (num <= n * n)
+{
+    matrix[a, b] = num;
+    if (a <= b + 1 && a + b < n - 1)
+        ++b;
+    else if (a < b && a + b >= n - 1)
+        ++a;
+    else if (a >= b && a + b > n - 1)
+        --b;
+    else
+        --a;
+    ++num;
+}
+for (int i = 0; i< n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+      Console.Write(matrix[i,j] + " \t");
+    }
+    Console.WriteLine();
+}
